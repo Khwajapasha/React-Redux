9793f31957a50778fda49820a6690a937cf881db
khwajapasha141@gmail.com <71385280+Khwajapasha@users.noreply.github.com>
2021-02-17 13:08:07 +0530

    remane the default value of state

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -4,39 +4,10 @@ import CounterControl from "../../components/CounterControl/CounterControl";
 import CounterOutput from "../../components/CounterOutput/CounterOutput";
 
 class Counter extends Component {
-  state = {
-    counter: 0,
-  };
-
-  counterChangedHandler = (action, value) => {
-    switch (action) {
-      case "inc":
-        this.setState((prevState) => {
-          return { counter: prevState.counter + 1 };
-        });
-        break;
-      case "dec":
-        this.setState((prevState) => {
-          return { counter: prevState.counter - 1 };
-        });
-        break;
-      case "add":
-        this.setState((prevState) => {
-          return { counter: prevState.counter + value };
-        });
-        break;
-      case "sub":
-        this.setState((prevState) => {
-          return { counter: prevState.counter - value };
-        });
-        break;
-    }
-  };
-
   render() {
     return (
       <div>
-        <CounterOutput value={this.props.ctr} />
+        <CounterOutput value={this.props.counterDefaultValue} />
         <CounterControl
           label="Increment"
           clicked={this.props.onIncrementCounter}
@@ -59,7 +30,7 @@ class Counter extends Component {
 const mapStateToProps = (state) => {
   console.log("state====--->>", state);
   return {
-    ctr: state.counter,
+    counterDefaultValue: state.counter,
   };
 };
 
