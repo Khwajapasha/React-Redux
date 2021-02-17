6ccd3c673c294c402cd87ea43576a716caa9f743
khwajapasha141@gmail.com <71385280+Khwajapasha@users.noreply.github.com>
2021-02-17 12:23:31 +0530

    working with redux

--- src/State/Reducers/counterReducer.js
+++ src/State/Reducers/counterReducer.js
@@ -2,7 +2,7 @@ const initialState = {
   counter: 0,
 };
 
-const counterReducer = (state, action) => {
+const counterReducer = (state = initialState, action) => {
   return state;
 };
 export default counterReducer;
--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 import { createStore } from "redux";
+import counterReducer from "./State/Reducers/counterReducer";
+
+const store = createStore(counterReducer);
 
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
