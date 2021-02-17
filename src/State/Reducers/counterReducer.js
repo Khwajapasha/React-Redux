c3c989439a26c240d117dd07d522abb38247dbcd
khwajapasha141@gmail.com <71385280+Khwajapasha@users.noreply.github.com>
2021-02-17 12:49:57 +0530

    added action to counterReducer

--- src/State/Reducers/counterReducer.js
+++ src/State/Reducers/counterReducer.js
@@ -3,6 +3,11 @@ const initialState = {
 };
 
 const counterReducer = (state = initialState, action) => {
+  if (action.type === "INCREMENT") {
+    return {
+      counter: state.counter + 1,
+    };
+  }
   return state;
 };
 export default counterReducer;
